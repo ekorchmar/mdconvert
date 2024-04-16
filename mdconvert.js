022e30455ee2312d5a0d6cb3cb50c53abb05d377
Eduard Korchmar <eduard.korchmar@odysseusinc.com>
2024-04-16 13:46:22 +0200

    Initial commit

---
+++ mdconvert.js
@@ -0,0 +1,38 @@
+const showdown = require('showdown'),
+      fs = require('fs');
+const converter = new showdown.Converter();
+let html;
+
+// Read args
+if (process.argv.length !== 3) {
+  console.log('Usage: node index.js <markdown-file>');
+  process.exit(1);
+}
+
+const markdownFile = process.argv[2];
+let htmlFile = markdownFile.replace(/\.md$/i, '.html');
+console.debug('input: ' + markdownFile);
+
+// If filename turns out the same:
+if (markdownFile === htmlFile) {
+  htmlFile = markdownFile + '.html';
+  console.warn('Warning: input and output file are the same. Saving to ' + htmlFile);
+}
+console.debug('output: ' + htmlFile);
+
+try {
+  const data = fs.readFileSync(markdownFile, 'utf8');
+  html = converter.makeHtml(data);
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
+
+fs.writeFile(htmlFile, html, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log('Conversion complete. HTML saved to ' + htmlFile);
+});
+
---
+++ test.md
@@ -0,0 +1,38 @@
+# Test file
+
+## Testing
+### Different
+#### Headers
+
+- Testing
+- Unordered
+  - Including
+  - Nested
+- List
+
+1. Testing
+2. Ordered
+   1. Including
+   2. Nested
+3. List
+
+Testing **bold** and *italic* text.
+
+```python
+    print("Testing code block")
+```
+
+Testing [links](https://www.google.com) and images ![image](https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png).
+
+> Testing blockquote
+
+# Testing tables
+
+| Column1    | Column2    | Column3    |
+|---------------- | --------------- | --------------- |
+| Item1.1    | Item2.1    | Item3.1    |
+| Item1.2    | Item2.2    | Item3.2    |
+
+- [ ] Testing task list
+- [x] Completed task list
+
